5ad20708ee99e18a9b8bc62794beaa42e8fd7f62
Anushanallap <anusha.n468@gmail.com>
2022-01-28 12:57:08 +0530

    Added email address using regex

--- Email/Email.js
+++ Email/Email.js
@@ -4,6 +4,7 @@
  function emailaddress(emailvalid,emailinvalid)
 {
     let pattern = RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+    let pattern = RegExp ('[A-Za-z0-9.+-]*[a-zA-Z0-9]+[@]{1}[b][r][i][d][g][e][l][a][b][z]+[.][a-z]');
 
     let email = pattern.test(emailvalid);
     let email1 = pattern.test(emailinvalid);
@@ -12,3 +13,8 @@
     console.log(email1);  
 }
 emailaddress("abc@bridgelabz.co","ab");
+
+/**UC2 ensure @ and validate the mandatory 2nd part i.e bridgelabz
+ * 
+ */
+emailaddress("bridgelabz.co");
